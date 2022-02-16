4b112c6f5619c511af6458ef71633aac79008569
Josh <joshualaikowski@gmail.com>
2022-02-16 14:17:09 -0500

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "static_pages#root"
 
-  namspace :api, defaults: {format: :json} do
+  # namspace :api, defaults: {format: :json} do
 
-  end
+  # end
 end
