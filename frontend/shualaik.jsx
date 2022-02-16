adf2ffec2f13141023de949d7c81a432ed264fb3
Josh <joshualaikowski@gmail.com>
2022-02-16 15:58:43 -0500

    Fixing webpacker related  issues

--- frontend/shualaik.jsx
+++ frontend/shualaik.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-console.log("hit!")
-document.addEventListener("DOMContentLoaded", () => {
 
+document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
   ReactDOM.render(<h1>Welcome to ShuaLaik</h1>, root);
 });
