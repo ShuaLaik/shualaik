9fed62a0c606d1cf01554ed2bbdfd32fce9f6bf7
Josh <joshualaikowski@gmail.com>
2022-02-17 15:37:28 -0500

    Added nav bar

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,16 +3,16 @@ import { Route, Switch } from "react-router-dom"
 import { Routes } from "react-router";
 import HeaderContainer from "./header/header_container";
 import Demo1 from "./demo1";
-import Demo2 from "./demo1";
+import Demo2 from "./demo2";
 // import { AuthRoute, ProtectedRoute } from "../util/route_util";
 
 
 const App = ( { store } ) => {
-    return (<div>
+    return (<div id="body">
         <Routes>
             <Route path="/" element={<HeaderContainer />} >
-                <Route path="/demo1" element={<Demo1 />} />
-                <Route path="/demo2" element={<Demo2 />} />
+                <Route path="demo1" element={<Demo1 />} />
+                <Route path="demo2" element={<Demo2 />} />
             </Route>
         </Routes>
     </div>)
--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import Demo1 from '../demo1'
+import Demo2 from '../demo2'
 
 export default class Header extends Component {
   render() {
     return (
-      <div>
-          <h1>Working</h1>
+      <div id="header">
+          <ul>
+              <Link to="">About</Link>
+              <Link to="">Music</Link>
+              <Link to="">Production</Link>
+              <Link to="">Modeling</Link>
+              <Link to="">Web Dev</Link>
+              <Link to="">Social Medias</Link>
+          </ul>
       </div>
     )
   }
