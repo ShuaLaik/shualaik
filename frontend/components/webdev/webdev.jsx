392de9e857a13647dcb616db7bbabee291b538d3
Josh <joshualaikowski@gmail.com>
2022-03-09 17:20:03 -0500

    Added Picture

--- frontend/components/webdev/webdev.jsx
+++ frontend/components/webdev/webdev.jsx
@@ -3,7 +3,12 @@ import React, { Component } from 'react'
 export default class Webdev extends Component {
   render() {
     return (
-      <div>Webdev</div>
+      <div id="webdev">
+        <div id="left-bar-webdev">
+          <img src={window.resumeURL}/>
+        </div>
+        
+      </div>
     )
   }
 }
