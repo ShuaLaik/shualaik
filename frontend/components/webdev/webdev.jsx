30b97314795fa151b363a52e33de5702c5063b6a
Josh <joshualaikowski@gmail.com>
2022-03-09 18:27:51 -0500

    Solid Progress

--- frontend/components/webdev/webdev.jsx
+++ frontend/components/webdev/webdev.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Outlet } from 'react-router'
 
 export default class Webdev extends Component {
   render() {
@@ -6,8 +7,17 @@ export default class Webdev extends Component {
       <div id="webdev">
         <div id="left-bar-webdev">
           <img src={window.resumeURL}/>
+          <h1>Joshua Laikowski</h1>
+          <h2>Full Stack Engineer</h2>
+          <p>Hi! I'm Josh, a Web Developer and Software Engineer by day, Musician by night.</p>
+          <p>I'm fluent in Javascript, React, Redux, Ruby, Ruby on Rails, MongoDB, Node.js, and more. I recently finished the curriculum at App Academy, a highly selective and immersive Software Engineering boot camp in the NYC area, where I spent 1000 hours refining my skills and exposing myself to cutting-edge technologies.
+          </p>
+        <p>
+          When I'm not programming, you can find me emersing myself in music, whether it be performing, writing, producing, etc.</p>
         </div>
-        
+        <div id="right-bar-webdev">
+          <Outlet />
+        </div>
       </div>
     )
   }
